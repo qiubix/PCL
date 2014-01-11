124f6a42f4178e9ca907f2aa5259bec990ce4490
Michał Laszkowski <mlaszkow@tola.ia.pw.edu.pl>
2014-01-11 16:49:27 +0100

    Downsampling

--- src/Components/Downsampling/Downsampling.cpp
+++ src/Components/Downsampling/Downsampling.cpp
@@ -60,38 +60,59 @@ void Downsampling::downsample_xyzsift() {
 	kdtree.setInputCloud (cloud);
 	PointXYZSIFT searchPoint;
 	
-	
-	cout<<"Cloud size: "<<cloud->size()<<endl;
  // Neighbors within radius search
   std::vector<int> pointIdxRadiusSearch;
   std::vector<float> pointRadiusSquaredDistance;
 
-  int t;//times
-  int s = 0;
+	int t;//times
+	cout<<"cloud size: "<<cloud->size()<<endl;
+	//zliczenie krotnosci
+	//0 - punkt nie ma jeszcze policzonej krotnosci
+	//-1 - punkt do usuniecia
   pcl::PointCloud<PointXYZSIFT>::iterator pt_iter = cloud->begin();
- // while(pt_iter!=cloud->end()){
-
+  while(pt_iter!=cloud->end()){
+	
 	  searchPoint = *pt_iter++;
+	  if(searchPoint.times!=0)
+		continue;
 	  t = kdtree.radiusSearch (searchPoint, radius, pointIdxRadiusSearch, pointRadiusSquaredDistance);
-	  cout<< "Times: "<<t<<endl;
-	  s+=t;
-	  cout<<"s: "<<s<<endl;
+
+
 	  if ( t > 0 )
 	  {
-		for (size_t i = 0; i < pointIdxRadiusSearch.size (); ++i){
-			
-			if (i==0)
-				cloud->points[ pointIdxRadiusSearch[i] ].times = t;
-			else	
-				cloud->erase(cloud->begin() + pointIdxRadiusSearch[i]);//-1??
+		int wrongPoints = 0;
+		for (size_t i = 0; i < t; ++i){
+			if(cloud->points[ pointIdxRadiusSearch[i] ].times ==0)
+				cloud->points[ pointIdxRadiusSearch[i] ].times = -1;
+			else //punkt zostal juz usuniety/zliczony wczesniej
+				wrongPoints++;
+				//cloud->erase(cloud->begin() + pointIdxRadiusSearch[i]);//-1??
 		  //std::cout << "    "  <<   cloud->points[ pointIdxRadiusSearch[i] ].x 
 					//<< " " << cloud->points[ pointIdxRadiusSearch[i] ].y 
 					//<< " " << cloud->points[ pointIdxRadiusSearch[i] ].z 
 					//<< " (squared distance: " << pointRadiusSquaredDistance[i] << ")" << std::endl;
-
 		}
+		cloud->points[ pointIdxRadiusSearch[0] ].times = t-wrongPoints;
+		//cout<< "t: " <<t;
+		//if(wrongPoints>0)
+			//cout<<" Wrong points: "<<wrongPoints;
+		//cout<<endl;
 	  }
- //}
+ }
+
+	//usuniecie nadmiarowych punktow
+	pt_iter = cloud->begin();
+	 while(pt_iter!=cloud->end()){
+		 if(pt_iter->times==-1 || pt_iter->times==0){
+			 pt_iter = cloud->erase(pt_iter);
+		}
+		else{
+			++pt_iter;	
+		}
+		
+	}
+	cout<<"cloud size: "<<cloud->size()<<endl;
+	
 	out_cloud_xyzsift.write(cloud);
 }
 
